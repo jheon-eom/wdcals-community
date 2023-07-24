5377e20832653dcd2eda3844ab23150671c14e25
jheon-eom <https://github.com/jheon-eom>
2023-07-24 21:20:47 +0900

    Board 테이블 삭제여부 컬럼 추가

--- src/main/java/com/wdcals/wdCommunity/entity/Board.java
+++ src/main/java/com/wdcals/wdCommunity/entity/Board.java
@@ -10,6 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Board extends BaseEntity {
+    /**
+     * 필드 (테이블 컬럼)
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -20,14 +23,25 @@ public class Board extends BaseEntity {
     @Column
     private String content;
 
+    @Column
+    private boolean isDeleted;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
+    /**
+     * 생성자 & 팩토리 메소드
+     */
     @Builder
-    public Board(Long id, String title, String content, Member member) {
+    public Board(Long id, String title, String content, boolean isDeleted, Member member) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.isDeleted = isDeleted;
         this.member = member;
     }
+
+    /**
+     * 비즈니스 로직
+     */
 }
